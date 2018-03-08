80e19610bfce82915dd2f469cac3409401c06550
Tomasz Kalinowski <tkalinow@asu.edu>
2018-03-08 00:18:13 -0500

    more WIP split performance

--- R/split.R
+++ R/split.R
@@ -46,10 +46,11 @@ split_on_dim <- function(X, .dim,
   if(is.list(f))
     f <- interaction(f, drop = TRUE)
 
-
   if (is.list(X) && is.null(dim(X)) && depth > 0L)
     return(lapply(X, function(x) split_on_dim(x, .dim, f = f, drop = drop, depth = depth - 1L)))
 
+  if (is.character(.dim))
+    .dim <- match(.dim, names(dimnames(X)))
 
   if(.dim == 1 && dim(X)[1L] > 500L) {
     X <- aperm(X, c(2:length(dim(X)), 1L))
@@ -70,9 +71,11 @@ split_on_dim <- function(X, .dim,
   if (!identical(length(id), length(f)))
     stop("`f` must be the same length as the dimension being split on.")
 
+  expr <- extract_dim_chr_expr(X, .dim, .idx_var = "idx", drop = drop)
+  expr <- parse(text = expr)[[1]]
+
   l <- split(id, f)
-  lapply(l, function(idx)
-    extract_dim(X, .dim, idx, drop = drop, depth = 0L))
+  lapply(l, function(idx) eval(expr))
 }
 
 #' @rdname split-array
@@ -101,33 +104,28 @@ split_along_dim <- function(X, .dim, drop = NULL, .keep_names = TRUE, depth = In
       split_along_dim(x, .dim, drop = drop, .keep_names = .keep_names, depth = depth - 1L)))
 
 
-  if(.dim == 1 && length(dim(X)) >= 3L && dim(X)[1L] > 500L) {
-    # subsetting on first index is oom slower than last index for large arrays
+  if (is.character(.dim))
+    .dim <- match(.dim, names(dimnames(X)))
+
+
+  if(.dim == 1L && length(dim(X)) >= 3L && dim(X)[1L] > 500L) {
+    # subsetting on first index is OOM slower than on last index for large arrays
     # due to F style (column major) ordering of arrays.
-    # aperm has a very fast strided slice in C
+    # aperm() has a very fast strided slice in C
     X <- aperm(X, c(2:length(dim(X)), 1L))
+    # dim(X) <- c(prod(dim(X)[-length(dim(X))]), dim(X)[length(dim(X))])
     .dim <- length(dim(X))
   }
 
-  expr <- extract_dim_chr_expr(X, .dim, .idx_var = "i", drop = drop)
+  expr <- extract_dim_chr_expr(X, .dim, .idx_var = "i",
+                               drop = drop, .var_to_subset = "Xin")
+
+  # expr <- paste("array(", expr, ", c(256, 4))")
+
   expr <- parse(text = expr)[[1]]
 
-  # out <- vector("list", dim(X)[.dim])
-  # # browser()
-  #
-  # for (i in seq_along_dim(X, .dim)) {
-  #   # out[[i]] <- X[,,i]
-  #   out[[i]] <- eval(expr)
-  # }
+  out <- lapply(seq_along_dim(X, .dim), function(i, Xin) eval(expr), Xin = X)
 
-  out <- lapply(seq_along_dim(X, .dim),
-                function(i) eval(expr))
-
-
-  # out <- lapply(seq_along_dim(X, .dim),
-  #               function(i)
-  #                 extract_dim(X, .dim, i, drop = drop, depth = 0L))
-  #
   if (isTRUE(.keep_names) && !is.null(nms <- dimnames(X)[[.dim]]))
     names(out) <- nms
 
@@ -137,54 +135,9 @@ split_along_dim <- function(X, .dim, drop = NULL, .keep_names = TRUE, depth = In
 #' @rdname split-array
 #' @export
 split_along_rows <-
-  function(X, drop = NULL, .keep_names = TRUE, depth = Inf) {
+  function(X, drop = NULL, .keep_names = TRUE, depth = Inf)
     split_along_dim(X, 1L, drop = drop, .keep_names = .keep_names, depth = depth)
-    # out <- unlist(apply(X, 1, list), recursive = FALSE)
-    # lapply(out, drop_dimnames)
-    # out
-  }
 
-
-
-
-# > dim(X)
-# [1] 368000    128      2
-# tl1 <- system.time(rl1 <- unlist(apply(X, 1, list), recursive = FALSE) %>% lapply(drop_dimnames))
-# tl2 <- system.time(rl2 <-  split_along_rows(X))
-# > identical(rl1, rl2)
-# [1] TRUE
-# > tl1
-# user  system elapsed
-# 3.027   0.297   3.301
-# > tl2
-# user  system elapsed
-# 55.272   0.297  55.194
-
-# it's because `[` is so slow for arrays
-# Xa <- test$X
-# Xm <- Xa
-# dim(Xm) <- c(dim(X)[1], prod(dim(X)[-1]))
-# tXm <- t(Xm)
-# tXa <- Xa
-# dim(tXa) <- rev(dim(Xa))
-#
-# library(microbenchmark)
-# autoplot(microbenchmark(
-#   Xa[6,,],
-#   Xm[6,],
-#   tXm[,6],
-#   tXa[,,6]
-# ))
-# Unit: microseconds
-# expr        min        lq    mean  median     uq    max neval cld
-# Xa[6, , ]   3.317 3.6125 4.10827 3.8530 4.0535 16.036   100   c
-# Xm[6, ]     1.460 1.5690 2.07374 1.8020 1.8850 16.223   100 a
-# tXm[, 6]    1.490 1.5720 1.67813 1.6185 1.6955  2.747   100 a
-# tXa[, , 6]  2.210 2.4320 2.62682 2.4965 2.6385  5.238   100  b
-#
-#
-#apply() forces things into a matrix, then restores dim (maybe) after looping
-#with f() on all elements
 
 
 #' @rdname split-array
@@ -192,4 +145,6 @@ split_along_rows <-
 split_along_cols <-
   function(X, drop = NULL, .keep_names = TRUE, depth = Inf)
     split_along_dim(X, 2L, drop = drop, .keep_names = .keep_names, depth = depth)
+
+
 
