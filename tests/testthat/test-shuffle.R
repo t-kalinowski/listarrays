3f898ed8281e20826e2c8acf8cd681a15457c8a2
Tomasz Kalinowski <tkalinow@asu.edu>
2018-05-30 20:37:14 -0400

    remove test for shuffle_rows(in_sync = FALSE)

--- tests/testthat/test-shuffle.R
+++ tests/testthat/test-shuffle.R
@@ -13,11 +13,4 @@ test_that("shuffle_rows", {
   expect_identical(y[i,],  l[[2]])
   expect_identical(z[i,,], l[[3]])
 
-  set.seed(1)
-  l <- shuffle_rows(x, y, z, in_sync = FALSE)
-  expect_identical(x[c(1,3,2)],   l[[1]])
-  expect_identical(y[c(3,1,2),],  l[[2]])
-  expect_identical(z[c(3,2,1),,], l[[3]])
-
-
 })
